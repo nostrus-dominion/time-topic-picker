690f0e39a57ee26528cbe42b35d26b39ad57a1b3
Admin of Nostrus-Dominion <admin@nostrus-dominion.co>
2024-05-26 10:35:27 -0400

    Updated topics

--- src/topics.js
+++ src/topics.js
@@ -1,13 +1,17 @@
 const topics = [
-  "Geography",
-  "Entertainment",
-  "History",
-  "Arts",
+  "News",
+  "Pop Culture",
+  "Fashion",
+  "Music",
+  "Movies",
+  "Television",
   "Literature",
   "Science",
+  "Technology",
   "Nature",
   "Sports",
   "Leisure"
+  "Fine Art",
 ];
 
 export default topics;
